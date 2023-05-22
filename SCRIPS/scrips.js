39f1ad576be82153e8b3c3dc13b5742a7c92ec52
DORIS GOMEZ <doris.mgv@gmail.com>
2023-05-22 00:47:57 -0300

    contacto y experiencia

---
+++ SCRIPS/scrips.js
@@ -0,0 +1,124 @@
+document.getElementById("idheader").innerHTML =`
+<nav class="navbar navbar-expand-lg navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/IMG/LOGO/Black_Minimal_Typographic_Brand_Name_Initials_Monogram_Logo-removebg-preview.png" class="a-img" alt="logo inicio"></a>
+  
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+      <li class=nav-link">
+        <a class="nav-link" aria-current="page" href="/index.html">INICIO </a> </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/PAGES/labodega.html#LABODEGA">LA BODEGA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/PAGES/experiencias.html">EXPERIENCIAS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/PAGES/vinos.html">VINOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/PAGES/galeria.html">GALERIA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/PAGES/contacto.html">CONTACTO</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+`
+
+
+
+document.getElementById("idfooter").innerHTML= `
+<footer class="bg-light text-dark pt-5 pb-4">
+    <div class="container text-center text-md-start">
+      <div class="row text-center text-md-start">
+
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+
+        <h5 class="text-uppercase mb-4 font-weight-bold text-primary"></h5>
+        
+        <a class="navbar-brand" href="#">
+          <img src="/IMG/LOGO/Black_Minimal_Typographic_Brand_Name_Initials_Monogram_Logo-removebg-preview.png " class="f-img" alt="logo de footer"></a >
+          
+      </div>
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 ">
+  <h5 class="text-uppercase mb-4 font-weight-bold text-primary"></h5>
+        
+          <p>
+            <a href="/PAGES/labodega.html" class="text-dark"></a>
+          </p>
+          <p>
+            <a href="#" class="text-dark"></a>
+          </p>
+          <p>
+            <a href="#" class="text-dark"></a>
+          </p>
+          <p>
+            <a href="#" class="text-dark"></a>
+          </p>
+        </div>
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 ">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-primary">HORARIO DE ATENCION</h5>
+       <h6>MIERCOLES A VIERNES</h6>
+            <p>
+            10:00am a 04:00pm
+            </p>
+        <h6>SABADO Y DOMINGOS </h6>
+            <P>
+              01:0pm a 5:00pm
+            </P>
+        <h6>CON TURNO RESERVA PREVIA</h6>
+          </div>
+          
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 ">
+            <h5 class="text-uppercase mb-4 font-weight-bold text-primary">CONTACTO</h5>
+            
+              <p>
+                <li class="fas fa-home me-3"></li>  CALLE LA ODISEA 6666, MENDOZA. ARGENTINA
+              </p>
+              <p>
+                <li class="fas fa-envelope me-3"></li> eltano@gmail.com 
+              </p>
+              <p>
+                <li class="fas fa-phone me-3"></li> (+54)911 3064-8763
+              </p>
+            </div>
+          </footer>
+
+
+<footer>
+            <div class="text-center mb-2">
+              <p>
+                Copyright Todos los derechos reservados. CUIT: 91-536426456-6
+                <a href="#">
+                  <strong class="text-primary">TERMINOS Y CONDICIONES</strong>
+                </a>
+              </p>
+            </div>
+
+            <div class="text-center mb-2"></div>
+              <ul class="list-unstyled  text-center list-inline">
+                <li class="list-inline-item">
+                  <a href="#" class="text-dark"><i class="fab fa-github"></i> </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#" class="text-dark"><i class="fab fa-whatsapp"></i> </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#" class="text-dark"><i class="fab fa-instagram"></i> </a>
+                </li>
+            </ul>
+      </div>  
+      </div>
+    </div>
+  </div>
+  </footer>
+  `
